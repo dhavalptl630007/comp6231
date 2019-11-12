c577ad88a1d53717d70da4b107405828bd7558a0
dhavalptl630007 <dhavalptl630007@gmail.com>
2019-11-12 17:20:03 -0500

    Dhaval : rules added

--- src/main/java/phising/Phising.java
+++ src/main/java/phising/Phising.java
@@ -15,14 +15,21 @@ public class Phising {
 		
 		JavaRDD<String> textFile = sc.textFile("C:/Users/Admin/Desktop/html.txt");
 		
-		long count = textFile.filter(line->line.contains("<a href=\"#\"")).count();
 		
+		
+		int URL_of_Anchor = hasURLAnchor(textFile);
+		int Submitting_to_email = hasSubmittingToEmail(textFile);
+	    int Redirect = hasRedirect(textFile);
+	    int on_mouseover = hasOnMouseOver(textFile);
+	    int RightClick = hasRightClickDisabled(textFile);
+	    int Iframe = hasIframe(textFile);
+	    
 		List<String> list = textFile.collect();
 		for (String string : list) {
 			//System.out.println(string);
 		}
 		
-		System.out.println(count);
+		System.out.println(URL_of_Anchor);
 		
 		String url="http://netflix.hotmaster.dns-cloud.net/clienteseguro/site/index.html";
 		int outputHasIp = hasIp(url);
@@ -35,6 +42,86 @@ public class Phising {
 		int outputHasHttps = hasHttps(url);
 	}
 	
+
+	private static int hasURLAnchor(JavaRDD<String> textFile) {
+		
+		double numberOfAnchor = filterAndCount(textFile,"<a href");
+		double hasAAnchor = filterAndCount(textFile,"<a href=\"#\" ");
+		double hasContentAnchor = filterAndCount(textFile,"<a href=\"#content\" ");
+		double hasSkipAnchor = filterAndCount(textFile,"<a href=\"#skip\" ");
+		double hasJavascriptMethod = filterAndCount(textFile,"JavaScript ::void(0)");
+		
+		if(numberOfAnchor!=0) {
+			
+			double perURLAnchor = (hasAAnchor/numberOfAnchor)*100 + (hasContentAnchor/numberOfAnchor)*100
+					              + (hasSkipAnchor/numberOfAnchor)*100 
+				                  + (hasJavascriptMethod/numberOfAnchor)*100 ;
+			
+			if(perURLAnchor <31) {
+				return 1;
+			}
+			else if (perURLAnchor >=31 && perURLAnchor >=67) {
+				return 0;
+			}
+			else {
+				return -1;}
+			}
+
+		return 1;
+		}
+		
+	
+
+
+	private static int hasIframe(JavaRDD<String> textFile) {
+		if (filterAndCount(textFile,"iframe")>0 ) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
+
+	private static int hasRightClickDisabled(JavaRDD<String> textFile) {
+		if (filterAndCount(textFile,"onMouseOver")>0 ) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
+
+	private static int hasOnMouseOver(JavaRDD<String> textFile) {
+		if (filterAndCount(textFile,"event.button==2")>0 ) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
+
+	private static int hasRedirect(JavaRDD<String> textFile) {
+
+		if(filterAndCount(textFile,"window.location")>0 ||filterAndCount(textFile,"<meta http-equiv=\"refresh\" ") >0 ) {
+			return -1;
+			}
+			else
+			{
+				return 1;	
+			}
+	}
+
+
+	private static int hasSubmittingToEmail(JavaRDD<String> textFile) {
+		
+		if(filterAndCount(textFile,"mail()")>0 ||filterAndCount(textFile,"mailto") >0 ) {
+		return -1;
+		}
+		else
+		{
+			return 1;	
+		}
+	}
 
 	private static int hasHttps(String url) {
 		String [] parts = url.split("//");
@@ -115,5 +202,9 @@ public class Phising {
 		
 	}
 
-
+   private static long filterAndCount(JavaRDD<String> textFile, String pattern) {
+	
+	 return  textFile.filter(line->line.contains(pattern)).count();
+	   
+   }
 }
